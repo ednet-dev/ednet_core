efe22758ebc0d1df642f1c2e05372a0e3e215cf4
slavisam <slavisam@gmail.com>
2023-01-13 10:53:23 +0100

    Comment out broken code.

--- lib/ednet_core.dart
+++ lib/ednet_core.dart
@@ -2,5 +2,5 @@ library ednet_core;
 
 /// Basic infrastructure for the Domain-Driven Design (DDD) approach.
 export 'src/domain/model.dart';
-export 'src/domain/infrastructure.dart';
-export 'src/configuration/bootstrap.dart';
+// export 'src/domain/infrastructure.dart';
+// export 'src/configuration/bootstrap.dart';
